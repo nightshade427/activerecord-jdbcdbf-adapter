a4700d24a76104161cd91e4dfe987c724be5e043
nightshade427 <nightshade427@gmail.com>
2010-07-09 14:24:04 -0700

    code cleanup. fixed issue with limit

--- lib/active_record/connection_adapters/jdbcdbf_adapter.rb
+++ lib/active_record/connection_adapters/jdbcdbf_adapter.rb
@@ -24,23 +24,32 @@ module JdbcSpec
     def self.adapter_matcher(name, *)
       name =~ /dbf/i ? self : false
     end
-    def add_limit_offset!(sql, options) # :nodoc:
+    def add_limit_offset!(sql, options)
       if options[:limit]
         find_select = /\b(SELECT(?:\s+DISTINCT)?)\b(.*)/i
         whole, select, rest = find_select.match(sql).to_a
-        "#{select} TOP #{options[:lmit]} #{rest}"
+        sql.replace "#{select} TOP #{options[:limit]} #{rest}"
       end
     end
     def columns(table_name, name = nil)
-      super.each { |column| column.name.downcase! }
+      columns = super
+      downcase_columns columns
     end
     def select(sql, name=nil)
-      (super sql, name).map { |row| row.inject({}) { |new_row, pair| new_row[pair[0].downcase] = pair[1]; new_row } }
+      rows = super sql, name      
+      downcase_rows rows
     end
     def quoted_true; 'false'; end
     def quoted_false; 'true'; end
     def supports_migrations?; false; end
-    def quoted_date(value); "#{value.strftime("%Y-%m-%d")}"; end        
+    def quoted_date(value); "#{value.strftime("%Y-%m-%d")}"; end
+    private
+    def downcase_columns(columns)
+      columns.each { |column| column.name.downcase! }
+    end
+    def downcase_rows(rows)
+      rows.map { |row| row.inject({}) { |new_row, pair| new_row[pair[0].downcase] = pair[1]; new_row } }
+    end    
   end
 end
 
